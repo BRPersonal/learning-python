f6c1b6e519b084242aa6a3d0a311f352ca9e05dd
BRPersonal <balajirengan@gmail.com>
2025-04-02 13:07:08 +0530

    fixed asertion error

--- notice_email_extract.py
+++ notice_email_extract.py
@@ -5,6 +5,9 @@ import json
 from custom_json_encoder import CustomJSONEncoder
 
 
+#Field is optional and it provides metadata about the field.
+#exclude - When set to True, this parameter indicates that the field should be excluded from the model's serialization.
+#repr - When set to False, this parameter indicates that the field should not be included in the model's string representation.
 class NoticeEmailExtract(BaseModel):
     date_of_notice_str: str | None = Field(
         default=None,
@@ -53,14 +56,22 @@ if __name__ == "__main__":
         entity_name="Entity Name",
         max_potential_fine=1000.0,
     )
+    print(f"notice_email_extract: {notice_email_extract}")
 
     #serialize to json string
     notice_str = notice_email_extract.model_dump_json()
     print(f"notice_str: {notice_str}")
 
     #deserialize from json string
-    notice_email_extract_from_json = NoticeEmailExtract.model_validate_json(notice_str)
+    #convert the notice_str to json dictionary
+    notice_dict = json.loads(notice_str)
+    #set the date_of_notice_str to date_of_notice
+    notice_dict['date_of_notice_str'] = notice_dict['date_of_notice']
+
+    notice_email_extract_from_json = NoticeEmailExtract.model_validate_json(json.dumps(notice_dict))
     print(f"notice_email_extract_from_json: {notice_email_extract_from_json}")
+    print(f"type(notice_email_extract_from_json): {type(notice_email_extract_from_json)}")
+    print(f"type(notice_email_extract_from_json.date_of_notice): {type(notice_email_extract_from_json.date_of_notice)}")
 
     #check if the two objects are equal
     assert notice_email_extract == notice_email_extract_from_json
